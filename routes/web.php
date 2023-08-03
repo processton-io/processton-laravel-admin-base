2dcb3a01775176d31ee362b40068d89c88ceed11
processton <ahmadkokab@processton.com>
2023-08-03 23:32:21 +0500

    Route Issue

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
